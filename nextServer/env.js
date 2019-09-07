b45d3c7caa98e9ca0c795d4d3a96be21e2b63300
darnyelx <chiemeke.ifeanyi@gmail.com>
2019-09-07 23:08:01 +0100

    updated env file

--- nextServer/env.js
+++ nextServer/env.js
@@ -1,11 +1,7 @@
 module.exports =  {
-<<<<<<< HEAD
-    APP_PORT :3000,
-    API_URL  : 'https://cowfunding.com.ng',
-=======
+
     APP_PORT :3001,
-    API_URL  : 'https://localhost',
->>>>>>> feature-password-reset
+    API_URL  : 'http://stg.cowfunding.com.ng',
     API_PORT : 3009
 }
 
