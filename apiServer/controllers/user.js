a9be1c661ec81a0b508759c90ce23c0eafa12048
darnyelx <chiemeke.ifeanyi@gmail.com>
2019-09-07 23:38:39 +0100

    fixed bug om password reset

--- apiServer/controllers/user.js
+++ apiServer/controllers/user.js
@@ -1064,7 +1064,7 @@ Hello
 
   static passwordReset(req,res){
     let hashedPassword  = passwordHelper.hash(req.body.password);
-    let query = "UPDATE users SET password = ?, users.reset_key = ? WHERE uses.email = ? AND users.reset_key = ?";
+    let query = "UPDATE users SET password = ?, users.reset_key = ? WHERE users.email = ? AND users.reset_key = ?";
     let values = [hashedPassword,"null",req.body.email,req.body.reset_key];
     let data;
     return db.query(query,values).then((result)=>{
